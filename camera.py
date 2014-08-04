f9f48873ab9bc532864d9f844194ece64a0b88e6
Okky Jaya <okkificrada10@gmail.com>
2014-08-03 21:21:51 -0700

    adding camera.py

---
+++ camera.py
@@ -0,0 +1,12 @@
+#!/ENV/bin/python
+
+import time
+from SimpleCV import *
+
+# initialize left Camera specification such as resolution and video frames
+leftcamera = Camera(0, {"width": 1280, "height": 960})
+# initialize right Camera specification such as resolution and video frames
+rightcamera = Camera(1, {"width": 1280, "height": 960})
+
+leftcamera.getImage().save("left.jpg")
+rightcamera.getImage().save("right.jpg")
