87b400886c67f4a5755576af29e217b6319a2e70
Okky Jaya <okkificrada10@gmail.com>
2014-08-06 07:26:38 -0700

    edited the cameraJpeg.py

--- cameraJpeg.py
+++ cameraJpeg.py
@@ -1,5 +1,4 @@
 #!/env/bin/python
-
 import time
 from SimpleCV import *
 from SimpleCV.Display import Display
@@ -7,14 +6,10 @@ from SimpleCV.Display import Display
 # initialize left Camera specification such as resolution and video frames
 leftcamera = Camera(0, {"width": 1280, "height": 960})
 js = JpegStreamer("http://192.168.2.6:8080")
-
-while(1) 
-		leftcamera.getImage().save(js)
+leftcamera.getImage().save(js)
 
 # initialize right Camera specification such as resolution and video frames
 rightcamera = Camera(1, {"width": 1280, "height": 960})
 js = JpegStreamer("http://192.168.2.6:8090")
-
-while(1) 
-		rightcamera.getImage().save(js)
+rightcamera.getImage().save(js)
 
